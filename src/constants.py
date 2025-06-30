8ea3fb4ff0235fb828efbad21bdca7d4e967aeda
anishanup <anishanup26@gmail.com>
2025-06-30 11:39:17 -0500

    Basic setup

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+# This file holds paths to all folders as well as common constants used in this project
+
+TRAINED_MODELS_PATH = "trained_models"
+
+DATA_PATH = "data"
+DATA_IMAGES_PATH = DATA_PATH + "\\images"
+DATA_LABELS_PATH = DATA_PATH + "\\labels"
